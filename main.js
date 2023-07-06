1685c6f33d3a41a6b81494d0cace06bc4a824678
Sdney Lima <lima.rosimao@gmail.com>
2023-07-06 09:16:50 -0300

    feat: add input from date and time inmy-todo-list

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 //get content(value) from input
 const inputNewContent = document.getElementById("inputNewContent");
 
+//get content(value) from date and time from the exectution
+const formatTime = document.getElementById("timeTask");
+
+
 //get button and add event click
 const addNew = document.getElementById("addNew");
 
@@ -18,23 +22,27 @@ addNew.addEventListener("click", () => {
   const listItem = inputNewContent.value.trim();
   //listItem now it has of the new value
 
+  const timeValue = formatTime.value.trim();
+
   if (listItem !== "") {
     //create new element li inside UL#toDoList
     const newItem = document.createElement("li");
     newItem.classList.add("itemLi");
     newItem.textContent = listItem;
+    newItem.setAttribute("for", "forChecked");
 
-    //add content from listItem inside of the newItem
+    const itemTime = document.createElement("input");
+    itemTime.classList.add("inputTime");
+    itemTime.type = "datetime-local";
+    itemTime.value = timeValue;
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.classList.add("btnChecked");
-
-    const separator = document.createElement("hr");
-    separator.classList.add("separator");
+    checkbox.id = "forChecked";
 
     const remove = document.createElement("button");
-    remove.textContent = "Remove";
+    // remove.textContent = "Remove";
     remove.classList.add("btnRemove");
     remove.addEventListener("click", () => {
       newItem.remove();
@@ -42,27 +50,32 @@ addNew.addEventListener("click", () => {
       remove.remove();
       containerItem.remove();
     });
-
+    //icon trash of the input checked remove
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", "fa-trash-can");
+    remove.appendChild(icon);
+    //container that wrap all elements for styled
     const containerItem = document.createElement("div");
     containerItem.classList.add("containerItem");
 
     //add the newItem inside the toDoList element (ul)
     containerItem.appendChild(newItem);
+
+    containerItem.appendChild(itemTime);
     //add input checkbox inside of the toDoList (ul)
     containerItem.appendChild(checkbox);
     //add button delete inside of the newItem
     containerItem.appendChild(remove);
-    //remove content from inside of the inputNewContent and leave empty
     toDoList.appendChild(containerItem);
-
     inputNewContent.value = "";
+    timeTask.value = "";
   }
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
       const li = checkbox.parentElement;
       if (checkbox.checked) {
-        li.style.backgroundColor = "green";
+        li.style.backgroundColor = "greenyellow";
         li.style.textDecoration = "line-through";
         toDoListChecked.appendChild(li);
       } else {

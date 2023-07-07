91816575a0624921c4e502331c726a69326cf374
Junio Santos <git@junio.dev>
2023-07-07 13:16:47 -0300

    Add_upgrades

--- main.js
+++ main.js
@@ -1,121 +1,61 @@
-// ---------Code Constants---------->>>>>>
+// !! Reorganiza as declarações pra ficar tudo junto
+const addButton   = document.getElementById("Add");
 
-//get content(value) from input
-const inputNewContent = document.getElementById("inputNewContent");
+const inputTask   = document.getElementById("Description");
+const inputDate   = document.getElementById("Date");
 
-//get content(value) from date and time from the exectution
-const formatTime = document.getElementById("timeTask");
-let formatted
-formatTime.addEventListener("change", () => {
-  const formattedTime = formatsTime(formatTime.value);
-  formattedDate = formattedTime;
-});
+const listPending = document.getElementById("ListPending");
+const listChecked = document.getElementById("ListComplete");
 
-const formatsTime = (datetime) => {
-  const dateObj = new Date(datetime); 
+let itemId = 0;
 
-  const dia = dateObj.getDate();
-  const mes = dateObj.getMonth() + 1;
+inputDate.value = new Date().getUTCDate();
 
-  const hora = dateObj.getHours();
-  const minutos = dateObj.getMinutes();
+function addNewItem() {
+  const listItem = document.createElement("li");
+  listItem.class = "item";
+  listItem.id = `item-${itemId}`;
   
-  const dateFormated = `${dia
-    .toString()
-    .padStart(2, "0")}/${mes.toString().padStart(2, "0")}`;
-  const hoursFormated = `${hora
-    .toString()
-    .padStart(2, "0")}:${minutos.toString().padStart(2, "0")}`;
+  listItem.innerHTML(```
+    <div class="listDescription">
+      <p class="title">${inputTask.value}</p>
+      <p class="localtime">${inputDate.value}</p>
+    </div>
 
-  const timeFormated = `${dateFormated} - ${hoursFormated}`;
+    <div class="listActions">
+      <input 
+        id="checkbox-${itemId}" 
+        type="checkbox" 
+        class="checkbox" 
+        onclick="checkItem(${itemId})">
 
-  return timeFormated;
-};
+      <button 
+        onclick="deleteItem(${itemId})" 
+        class="delete fa-solid fa-trash-can">
+    </div>
+  ```);
 
-//get button and add event click
-const addNew = document.getElementById("addNew");
+  listPending.appendChild(listItem);
+  itemId += 1;
+}
 
-//get UL that receive new LI(listItem)
-const toDoList = document.getElementById("toDoList");
+function removeItem(id) {
+  document.removeChild(`item-${id}`);
+}
 
-const toDoListChecked = document.getElementById("toDoListChecked");
+function checkItem(id) {
+  const li = document.getElementById(`item-${id}`);
+  const checked = document.getElementById(`checkbox-${id}`).checked;
 
-//  ----------structure of the Code--------->>>>>
+  document.removeChild(li);
 
-addNew.addEventListener("click", () => {
-  //receiving content of the value from #inputNewContent
-  const listItem = inputNewContent.value.trim();
-  //listItem now it has of the new value
-
-  const timeValue = formatTime.value.trim();
-
-  if (listItem !== "") {
-    //create new element li inside UL#toDoList
-    const newItem = document.createElement("li");
-    newItem.classList.add("itemLi");
-    newItem.textContent = listItem;
-
-    const itemTime = document.createElement("li");
-    itemTime.classList.add("inputTime");
-    itemTime.textContent = formattedDate;
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("btnChecked");
-    // checkbox.id = "forChecked";
-
-    const remove = document.createElement("button");
-    const icon = document.createElement("i");
-    icon.classList.add("fa-solid", "fa-trash-can");
-    remove.appendChild(icon);
-    remove.classList.add("btnRemove");
-    remove.addEventListener("click", () => {
-      newItem.remove();
-      checkbox.remove();
-      remove.remove();
-      containerItem.remove();
-    });
-    //container that wrap all elements for styled
-    const containerRigth = document.createElement("div");
-    containerRigth.classList.add("rigthContainer");
-
-    const containerLeft = document.createElement("div");
-    containerLeft.classList.add("leftContainer");
-
-    const containerItem = document.createElement('div')
-    containerItem.classList.add('itemContainer')
-
-    //add the newItem inside of the containerItem
-    //add datetime inside of the containerItem
-    containerLeft.appendChild(newItem);
-    containerLeft.appendChild(itemTime);
-    //add input checkbox inside of the containerItem
-    containerRigth.appendChild(checkbox);
-    //add button remove inside of the containerItem 
-    containerRigth.appendChild(remove);
-    containerItem.appendChild(containerLeft)
-    containerItem.appendChild(containerRigth)
-    
-    //add all element inside of the containerItem for toDoList
-    toDoList.appendChild(containerItem);
-
-    inputNewContent.value = "";
-    timeTask.value = "";
+  if (checked) {
+    li.style.backgroundColor = "greenyellow";
+    li.style.textDecoration = "line-through";
+    listPending.appendChild(li);
+  } else {
+    li.style.backgroundColor = "#b0b0b0";
+    li.style.textDecoration = "underline";
+    listChecked.appendChild(li);
   }
-
-  const checkboxes = document.querySelectorAll("input[type=checkbox]");
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener("change", () => {
-      const li = checkbox.parentElement
-      if (checkbox.checked) {
-        li.style.backgroundColor = "greenyellow";
-        li.style.textDecoration = "line-through";
-        toDoListChecked.appendChild(li);
-      } else {
-        li.style.backgroundColor = "#b0b0b0";
-        li.style.textDecoration = "underline";
-        toDoList.appendChild(li);
-      }
-    });
-  });
-});
+}
